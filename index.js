0eaeeca29c77fc18cce6c5f0010613ef82414b8d
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-11-23 17:41:11 +0100

    Awesome font to Bootstrap CDN & background Audio

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
 $('.navbar-collapse a').click(function () {
     $(".navbar-collapse").collapse('hide');
 });
+
+var currentStatus = true;
+var src = document.getElementById("background-music").src;
+function playPauseAudio() {
+    if (currentStatus) {
+        currentStatus = false;
+        document.getElementById("playPauseIcon").innerHTML = '<i class="fa fa-volume-off"></i>';
+        document.getElementById("background-music").src = "";
+    } else {
+        currentStatus = true;
+        document.getElementById("playPauseIcon").innerHTML = '<i class="fa fa-volume-up"></i>';
+        document.getElementById("background-music").src = "https://www.youtube-nocookie.com/embed/scE8kZUv7hM?controls=1&autoplay=1";
+    }
+}
